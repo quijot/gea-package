910aa6764b5a7d097570ec79213fb771ece9d222
Santiago Pestarini <santiagonob@gmail.com>
2015-07-31 12:22:28 -0300

    v0.1.1
    Signed-off-by: Santiago Pestarini <santiagonob@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='gea',
-    version='0.1.0',
+    version='0.1.1',
     description='Gestión de Expedientes de Agrimensores para Django.',
     author='Santiago Pestarini',
     author_email='santiago@pestarini.com.ar',

86d866031f5a377419e30798fbdfc54d462dd4c9
quijoT <santiagonob@gmail.com>
2017-02-01 18:55:11 -0300

    v0.2.6
    Signed-off-by: quijoT <santiagonob@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='gea',
-    version='0.2.5',
+    version='0.2.6',
     description='Gestión de Expedientes de Agrimensores para Django.',
     author='Santiago Pestarini',
     author_email='santiago@pestarini.com.ar',

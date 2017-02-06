92803bbaa1e1426e40aa1a422f8a1a75d48084f1
quijoT <santiagonob@gmail.com>
2017-02-06 12:15:03 -0300

    v0.2.7
    Signed-off-by: quijoT <santiagonob@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='gea',
-    version='0.2.6',
+    version='0.2.7',
     description='Gestión de Expedientes de Agrimensores para Django.',
     author='Santiago Pestarini',
     author_email='santiago@pestarini.com.ar',
@@ -18,7 +18,8 @@ setup(
     long_description=open('README.rst', 'r').read(),
     install_requires=[
         'Django == 1.8.9',
-        'django-grappelli == 2.8.1',
+        'django-grappelli == 2.7.3',
+        'django-filebrowser == 3.6.4',
         'django-nested-admin == 2.1.8',
         'django-extensions == 1.7.6',
     ],   
